3d26569975e96279ab034bd61da0c230ec6e3669
Nicolas <nicolas__du95@hotmail.fr>
2022-06-29 18:16:14 +0200

    Dependencies

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -10,14 +10,13 @@ repo_key="https://repo.openwebrx.de/debian/key.gpg.txt"
 
 pkg_dependencies="python3 python3-pkg-resources \
 soapysdr-tools \
-wsjtx \
-debconf"
+wsjtx direwolf js8call \
+netcat debconf"
 
 extra_pkg_dependencies="csdr \
-netcat \
 owrx-connector hpsdrconnector \
-python3-js8py python3-csdr python3-digiham direwolf \
-js8call runds-connector aprs-symbols m17-demod nmux codecserver"
+python3-js8py python3-csdr python3-digiham \
+runds-connector aprs-symbols m17-demod nmux codecserver"
 
 #=================================================
 # DREAM
